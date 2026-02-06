54af121dbd48975964ef5c63ce51dcdc2ee9ce29
Yash Ghadage🌟 <YASH.21972@stu.upes.ac.in>
2026-02-06 21:51:21 +0530

    Add files via upload

---
+++ Q6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("%d ", arr[0]);
+
+    for (i = 1; i < n; i++) {
+        if (arr[i] != arr[i - 1]) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    return 0;
+}
+
